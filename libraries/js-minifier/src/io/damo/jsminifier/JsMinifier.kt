fa9ed7320b42f75112bee597f36eb3ab8a19e56f
Damien Le Berrigaud <dleberrigaud@pivotal.io>
2017-09-19 16:07:13 -0600

    Basic implementation.

--- libraries/js-minifier/src/io/damo/jsminifier/JsMinifier.kt
+++ libraries/js-minifier/src/io/damo/jsminifier/JsMinifier.kt
@@ -1,8 +1,41 @@
 package io.damo.jsminifier
 
+import com.google.javascript.jscomp.*
+import com.google.javascript.jscomp.CompilerOptions.Environment.BROWSER
+import java.io.File
+import java.nio.charset.StandardCharsets.UTF_8
+
 class JsMinifier {
 
     fun minify(inputPath: String, outputPath: String) {
-        TODO("This will minify JS file $inputPath and save it as $outputPath")
+        val compiler = Compiler()
+        val inputSourceFile = SourceFile.fromPath(pathFromString(inputPath), UTF_8)
+        val outputFile = File(outputPath)
+
+        val result = compiler.compile(externs, listOf(inputSourceFile), options)
+
+        when {
+            result.success -> {
+                outputFile.parentFile.mkdirs()
+                outputFile.writeText(compiler.toSource())
+            }
+
+            else -> {
+                val error = result.errors.joinToString(separator = "") {
+                    "${it.sourceName}:${it.lineNumber} - ${it.description}\n"
+                }
+                throw RuntimeException(error)
+            }
+        }
     }
+
+
+    private val externs = CommandLineRunner.getBuiltinExterns(BROWSER)
+
+    private val options = CompilerOptions().apply {
+        CompilationLevel.SIMPLE_OPTIMIZATIONS.setOptionsForCompilationLevel(this)
+        WarningLevel.QUIET.setOptionsForWarningLevel(this)
+    }
+
+    private fun pathFromString(filePath: String) = File(filePath).toPath()
 }
