1efcb19fdc79a9295e9311534188f932e3a144e8
Damien Le Berrigaud <dleberrigaud@pivotal.io>
2017-09-19 15:42:36 -0600

    Prepare project structure

---
+++ libraries/js-minifier/src/io/damo/jsminifier/JsMinifier.kt
@@ -0,0 +1,8 @@
+package io.damo.jsminifier
+
+class JsMinifier {
+
+    fun minify(inputPath: String, outputPath: String) {
+        TODO("This will minify JS file $inputPath and save it as $outputPath")
+    }
+}
